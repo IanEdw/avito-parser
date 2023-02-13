5605723a51987684a33614dd4eb48cf927357294
IanEdw <ianeduardvladimirovich@gmail.com>
2023-02-13 23:36:38 +0300

    test jsoup

--- src/main/java/ru/ianedw/avitoparser/parser/Parser.java
+++ src/main/java/ru/ianedw/avitoparser/parser/Parser.java
@@ -1,8 +1,7 @@
 package ru.ianedw.avitoparser.parser;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.interactions.Actions;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +13,20 @@ import ru.ianedw.avitoparser.models.Target;
 import ru.ianedw.avitoparser.services.TargetsService;
 import ru.ianedw.avitoparser.util.NotTargetPost;
 
+import java.io.IOException;
 import java.util.*;
 
 @Component
 public class Parser {
     Logger log = LoggerFactory.getLogger(Parser.class);
-    private final WebDriver browser;
-    private final JavascriptExecutor js;
-    private final Actions actions;
     private final TargetsService targetService;
     private final Map<Integer, Map<String, Post>> availablePosts;
-    private Map<Target, String> targetsWithTheirWindows;
-    private int yPointSamePosts;
-
+    private final String XPATH_QUERY = "//div[@data-marker='catalog-serp']//div[@data-marker='item']";
+    private Set<Target> targets;
 
 
     @Autowired
-    public Parser(WebDriver browser, Actions actions, TargetsService targetService) {
-        this.browser = browser;
-        this.js = (JavascriptExecutor) browser;
-        this.actions = actions;
+    public Parser(TargetsService targetService) {
         this.targetService = targetService;
         availablePosts = new HashMap<>();
         loadPosts(updateTargets());
@@ -45,56 +38,54 @@ public class Parser {
         return update;
     }
 
-    @Scheduled(initialDelay = 1000,fixedDelay = 10000)
+    @Scheduled(initialDelay = 1000, fixedDelay = 15000)
     private void updateAvailablePosts() {
         log.info("Запущено обновление объявлений");
         List<Target> newTargets = updateTargets();
         loadPosts(newTargets);
 
-        for (Target target : targetsWithTheirWindows.keySet()) {
+        for (Target target : targets) {
             log.info("Обновление цели " + target.getId());
             updatePosts(target);
         }
     }
 
     private void updatePosts(Target target) {
-        String xpathExpression = "//div[@data-marker='catalog-serp']//div[@data-marker='item']";
-        browser.switchTo().window(targetsWithTheirWindows.get(target));
-        WebElement findButton;
         try {
-            findButton = browser.findElement(By.xpath("//button[@data-marker='search-form/submit-button']"));
-            actions.click(findButton).build().perform();
-        } catch (NoSuchElementException | TimeoutException e) {
-            browser.get(target.getLink());
-        }
-        List<WebElement> postsOnPage = browser.findElements(By.xpath(xpathExpression));
-        log.info("На странице " + postsOnPage.size() + " объявлений");
-        yPointSamePosts = getYPointSamePosts();
+            Element body = Jsoup.connect(target.getLink()).get().body();
 
-        for (int i = 1; i < postsOnPage.size() + 1; i++) {
-            updatePost(xpathExpression, i, target.getId());
+            for (int i = 0; i < body.selectXpath(XPATH_QUERY).size(); i++) {
+                try {
+                    updatePost(body, i, target.getId());
+                } catch (NotTargetPost e) {
+                    break;
+                }
+            }
+            log.info("Размер availablePosts для Target " + target.getId() + " = " + availablePosts.get(target.getId()).size());
+        } catch (IOException e) {
+            log.warn("JSOUP НЕ СМОГ ПОЛУЧИТЬ ДАННЫЕ");
         }
-        log.info("Размер availablePosts для Target " + target.getId() + " = " + availablePosts.get(target.getId()).size());
     }
 
-    private void updatePost(String parentBlockXPathExpression, int blockNumber, int targetId) {
-        String expression = String.format(parentBlockXPathExpression + "[%d]", blockNumber);
-        String link = findPostLink(expression);
-        int blockYPoint = browser.findElement(By.xpath(expression)).getLocation().y;
+    private void updatePost(Element body, int blockNumber, int targetId) {
+        String expression = String.format(XPATH_QUERY + "[%d]", blockNumber + 1);
+        String link = findPostLink(body, expression);
         Map<String, Post> targetPosts = availablePosts.get(targetId);
 
-        if (blockYPoint > yPointSamePosts) {
-            return;
-        } else if (!isPostPublicationTimeValid(expression)) {
+        if (!isPostPublicationTimeValid(body, expression)) {
             targetPosts.remove(link);
+            throw new NotTargetPost();
+        }
+
+        if (targetPosts.containsKey(link)) {
             return;
         }
 
         Post post = new Post();
 
         post.setLink(link);
-        post.setName(findPostName(expression));
-        post.setPrice(findPostPrice(expression));
+        post.setName(findPostName(body, expression));
+        post.setPrice(findPostPrice(body, expression));
 
         targetPosts.put(link, post);
     }
@@ -106,43 +97,34 @@ public class Parser {
 
     private void loadPosts(Target target) {
         Map<String, Post> posts = new HashMap<>();
+        try {
+            Element body = Jsoup.connect(target.getLink()).get().body();
 
-        if (targetsWithTheirWindows.get(target) == null) {
-            targetsWithTheirWindows.put(target, openNewWindow());
-        }
-        browser.switchTo().window(targetsWithTheirWindows.get(target));
-        browser.get(target.getLink());
-
-        String xpathExpression = "//div[@data-marker='catalog-serp']//div[@data-marker='item']";
-        List<WebElement> postsOnPage = browser.findElements(By.xpath(xpathExpression));
-        yPointSamePosts = getYPointSamePosts();
-
-        for (int i = 1; i < postsOnPage.size() + 1; i++) {
-            try {
-                Post post = loadPost(xpathExpression, i);
-                posts.put(post.getLink(), post);
-            } catch (NotTargetPost ignored) {
-                break;
+            for (int i = 0; i < body.selectXpath(XPATH_QUERY).size(); i++) {
+                try {
+                    Post post = loadPost(body, i);
+                    posts.put(post.getLink(), post);
+                } catch (NotTargetPost e) {
+                    break;
+                }
             }
-        }
 
-        availablePosts.put(target.getId(), posts);
-        log.info("Загружено " + posts.size() + " объявлений");
+            availablePosts.put(target.getId(), posts);
+            log.info("Загружено " + posts.size() + " объявлений");
+        } catch (IOException e) {
+            log.warn("JSOUP НЕ СМОГ ПОЛУЧИТЬ ДАННЫЕ");
+        }
     }
-    private Post loadPost(String parentBlockXPathExpression, int blockNumber) {
-        String expression = String.format(parentBlockXPathExpression + "[%d]", blockNumber);
-        int blockYPoint = browser.findElement(By.xpath(expression)).getLocation().y;
 
-        if (blockYPoint > yPointSamePosts) {
-            throw new NotTargetPost();
-        }
+    private Post loadPost(Element body, int blockNumber) {
+        String expression = String.format(XPATH_QUERY + "[%d]", blockNumber + 1);
 
-        if (isPostPublicationTimeValid(expression)) {
+        if (isPostPublicationTimeValid(body, expression)) {
             Post post = new Post();
 
-            post.setName(findPostName(expression));
-            post.setLink(findPostLink(expression));
-            post.setPrice(findPostPrice(expression));
+            post.setName(findPostName(body, expression));
+            post.setLink(findPostLink(body, expression));
+            post.setPrice(findPostPrice(body, expression));
 
             return post;
         } else {
@@ -151,9 +133,9 @@ public class Parser {
     }
 
 
-    private boolean isPostPublicationTimeValid(String parentExpression) {
-        WebElement dateBlock = browser.findElement(By.xpath(parentExpression + "//div[@data-marker='item-date']"));
-        String[] dateArray = dateBlock.getText().split(" ");
+    private boolean isPostPublicationTimeValid(Element body, String parentExpression) {
+        String dateText = body.selectXpath(parentExpression + "//div[@data-marker='item-date']").text();
+        String[] dateArray = dateText.split(" ");
         String timeUnit = dateArray[1];
 
         switch (timeUnit) {
@@ -169,76 +151,33 @@ public class Parser {
         }
     }
 
-    private String findPostName(String parentExpression) {
-        return browser.findElement(By.xpath( parentExpression + "//a[@data-marker='item-title']")).getAttribute("title");
+    private String findPostName(Element body, String parentExpression) {
+        return body.selectXpath(parentExpression + "//a[@data-marker='item-title']").attr("title");
     }
 
-    private String findPostLink(String parentExpression) {
-        return browser.findElement(By.xpath( parentExpression + "//a[@data-marker='item-title']")).getAttribute("href");
+    private String findPostLink(Element body, String parentExpression) {
+        return "https://www.avito.ru" + body.selectXpath(parentExpression + "//a[@data-marker='item-title']").attr("href");
     }
 
-    private int findPostPrice(String parentExpression) {
-        return Integer.parseInt(browser.findElement(By.xpath(parentExpression + "//meta[@itemprop='price']")).getAttribute("content"));
+    private int findPostPrice(Element body, String parentExpression) {
+        return Integer.parseInt(body.selectXpath(parentExpression + "//meta[@itemprop='price']").attr("content"));
     }
-
 
 
     private List<Target> updateTargets() {
         List<Target> updatedList = targetService.getAll();
-        if (targetsWithTheirWindows == null) {
-            targetsWithTheirWindows = new HashMap<>();
-            updatedList.forEach(target -> targetsWithTheirWindows.put(target, null));
+        if (targets == null) {
+            targets = new HashSet<>(updatedList);
             return updatedList;
         }
 
         List<Target> newTargets = new ArrayList<>();
         for (Target target : updatedList) {
-            if (!targetsWithTheirWindows.containsKey(target)) {
-                targetsWithTheirWindows.put(target, null);
+            if (!targets.contains(target)) {
+                targets.add(target);
                 newTargets.add(target);
             }
         }
         return newTargets;
-    }
-
-    private int getYPointSamePosts() {
-        int result = Integer.MAX_VALUE;
-        WebElement samePosts = null;
-        WebElement samePostsInOtherCities = null;
-        try {
-            samePosts = browser.findElement(By.xpath("//div[text()='Похоже на то, что вы ищете']"));
-        } catch (NoSuchElementException ignored) {
-        }
-        try {
-            samePostsInOtherCities = browser.findElement(By.xpath("/div[contains(text(),'других городах')]"));
-        } catch (Exception ignored) {
-        }
-        if (samePosts != null) {
-            int samePostY = samePosts.getLocation().y;
-            if (samePostY < result) {
-                result = samePostY;
-            }
-        }
-        if (samePostsInOtherCities != null) {
-            int samePostsIntOtherCitiesY = samePostsInOtherCities.getLocation().y;
-            if (samePostsIntOtherCitiesY < result) {
-                result = samePostsIntOtherCitiesY;
-            }
-        }
-        return result;
-    }
-
-    private String openNewWindow() {
-        Set<String> openedWindows = browser.getWindowHandles();
-        js.executeScript("window.open()");
-        Set<String> newOpenedWindows = browser.getWindowHandles();
-
-        String newWindow = null;
-        for (String window : newOpenedWindows) {
-            if (!openedWindows.contains(window)) {
-                newWindow = window;
-            }
-        }
-        return newWindow;
     }
 }
